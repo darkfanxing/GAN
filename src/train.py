a365a12b57aa5a57069c5751d1e16da4c76f3798
darkfanxing <darkfanxing@gmail.com>
2021-11-07 00:28:07 +0800

    Remove unuse import package

--- src/train.py
+++ src/train.py
@@ -1,16 +1,13 @@
 from model import GAN
 from utils import check_and_use_gpus, get_mnist_images
 
-from tensorflow.keras.datasets import mnist
-import numpy as np
-
 if __name__ == "__main__":
     check_and_use_gpus(memory_limit=8192)
     training_data = get_mnist_images()
 
     gan = GAN(
         training_data=training_data,
-        epochs=50,
+        epochs=25,
         batch_size=32,
         optimizer="adam"
     )
