58ae76c12637114fa7e00770d7f26adec9b37068
darkfanxing <darkfanxing@gmail.com>
2021-11-08 15:29:04 +0800

    Add parameter name in watch_model_architecture.py

--- src/watch_model_architecture.py
+++ src/watch_model_architecture.py
@@ -1,5 +1,5 @@
 from tensorflow.python.keras.backend import log
 from utils import log_model_architecture
 
-log_model_architecture("src/model/trained_model/discriminator_example.h5")
-log_model_architecture("src/model/trained_model/generator_example.h5")
+log_model_architecture(model_path="src/model/trained_model/discriminator_example.h5")
+log_model_architecture(model_path="src/model/trained_model/generator_example.h5")
